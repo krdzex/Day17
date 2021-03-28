dbc3d5d8e6abe7deb44d6c3677f67bdf645da6a1
krdzex <58869690+krdzex@users.noreply.github.com>
2021-03-29 00:19:40 +0200

    Izmjena

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,7 @@
 const BAKING_TEMPERATURES = {
     cake: { low: 180, high: 250 },
     bread: { low: 150, high: 220 },
-    pie: { low: 200, high: 230 },
+    pie: { low: 200, high: 230 }
 };
 function main(baking) {
     // Only change code below this line
